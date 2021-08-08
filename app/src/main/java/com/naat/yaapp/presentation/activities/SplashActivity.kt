c6e58f9e39e1ef1dec605af760726e380f5d76a0
johnatitan <jonathan@MacBook-Pro-de-Jonathan.local>
2021-08-08 17:55:36 -0500

    ADD: Se agrega estructura de splashactivity. ADD: Se agrega estructura de loginactivity

--- app/src/main/java/com/naat/yaapp/presentation/activities/SplashActivity.kt
+++ app/src/main/java/com/naat/yaapp/presentation/activities/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.naat.yaapp.presentation.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.google.gson.JsonObject
 import com.naat.yaapp.R
 import com.titan.test.external.Service
@@ -10,21 +13,32 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.security.MessageDigest
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+        window.statusBarColor = getColor(R.color.main_blue)
 
-        val call = Service.endpoints.oAuthToken("password", "testnaat@na-at.com.mx", hashString("56Forj2018.", "SHA-256"))
+//        val call = Service.endpoints.oAuthToken("password", "testnaat@na-at.com.mx", hashString("56Forj2018.", "SHA-256"))
+//
+//        call.enqueue(object : Callback<JsonObject> {
+//            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+//                println(response.body().toString())
+//            }
+//
+//            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+//            }
+//        })
+    }
 
-        call.enqueue(object : Callback<JsonObject> {
-            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                println(response.body().toString())
-            }
+    override fun onResume() {
+        super.onResume()
 
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-            }
-        })
+        Handler(Looper.getMainLooper()).postDelayed({
+            val login = Intent(this, LoginActivity::class.java)
+            startActivity(login)
+            finish()
+        }, 5000)
     }
 
     private fun hashString(input: String, algorithm: String): String {
