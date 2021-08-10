9d3bee4cce3658e788d898bd23e01c7e0b89ce2a
johnatitan <jonathan@MacBook-Pro-de-Jonathan.local>
2021-08-10 00:03:08 -0500

    ADD: Se agrega EmptyFragment para tabs en contruccion. ADD: Se agrega estilo a texto de tabs

--- app/src/main/java/com/naat/yaapp/presentation/adapters/SectionsPagerAdapter.kt
+++ app/src/main/java/com/naat/yaapp/presentation/adapters/SectionsPagerAdapter.kt
@@ -4,18 +4,23 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.naat.yaapp.presentation.fragments.EmptyFragment
 import com.naat.yaapp.presentation.fragments.RechargeFragment
-
-private val TAB_TITLES = arrayOf(
-    "RECARGAS",
-    "RECAUDACIÓN",
-    "ADMINISTRACIÓN"
-)
 
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
+    private val TAB_TITLES = arrayOf(
+        "RECARGAS",
+        "RECAUDACIÓN",
+        "ADMINISTRACIÓN"
+    )
+
     override fun getItem(position: Int): Fragment {
-        return RechargeFragment.newInstance()
+        if (position == 0) {
+            return RechargeFragment.newInstance()
+        } else {
+            return EmptyFragment.newInstance()
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
