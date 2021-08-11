1d3acdf6e19276adab4354ab99f71557beee32e2
johnatitan <jonathan@MacBook-Pro-de-Jonathan.local>
2021-08-10 23:22:36 -0500

    ADD: Se agrega funcionalidad de filtrado de companhias

--- app/src/main/java/com/naat/yaapp/presentation/activities/BuyRechargeActivity.kt
+++ app/src/main/java/com/naat/yaapp/presentation/activities/BuyRechargeActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.naat.yaapp.R
 import com.naat.yaapp.data.models.Recharge
 import com.naat.yaapp.databinding.ActivityBuyRechargeBinding
-import com.naat.yaapp.domain.presenters.BuyRechargePresenter
+import com.naat.yaapp.domain.presenters.activities.BuyRechargePresenter
 import com.naat.yaapp.domain.utils.Constant
 import com.naat.yaapp.presentation.activities.views.BuyRechargeView
 import com.naat.yaapp.presentation.dialogs.ConfirmRechargeDialog
--- app/src/main/java/com/naat/yaapp/presentation/adapters/listeners/RechargeListener.kt
+++ app/src/main/java/com/naat/yaapp/presentation/adapters/listeners/RechargeListener.kt
@@ -4,4 +4,5 @@ import com.naat.yaapp.data.models.Recharge
 
 interface RechargeListener {
     fun showRechargesDialog(recharges: List<Recharge>)
+    fun showFailFilterMessage(message: String)
 }
